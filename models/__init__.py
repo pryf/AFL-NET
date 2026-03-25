1f070fd9558cc0564573a76ce3dda0743e2531c2
pryf <144296158+pryf@users.noreply.github.com>
2026-03-25 19:13:13 +0800

    Create __init__.py for models package
    Initialize the models package and define public API.

---
+++ models/__init__.py
@@ -0,0 +1,23 @@
+from .AFL_NET import (
+    SimAM,
+    MCAGate,
+    MHSA,
+    C2fSimAM,
+    C2f_GhostBottleneckV2,
+    SMC2f,
+    GhostNetV2,
+    GhostBottleneckV2,
+    ghostnetv2,
+)
+
+__all__ = [
+    "SimAM",
+    "MCAGate",
+    "MHSA",
+    "C2fSimAM",
+    "C2f_GhostBottleneckV2",
+    "SMC2f",
+    "GhostNetV2",
+    "GhostBottleneckV2",
+    "ghostnetv2",
+]
